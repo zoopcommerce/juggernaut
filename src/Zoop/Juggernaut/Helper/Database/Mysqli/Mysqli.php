a6cd79ebc80ceff0e1a58a66050dab3bf1c81174
crimsonronin <josh.stuart@zoopcommerce.com>
2014-01-20 09:36:51 +1100

    WIP added master and slave functions to allow multiple servers.

--- src/Zoop/Juggernaut/Helper/Database/Mysqli/Mysqli.php
+++ src/Zoop/Juggernaut/Helper/Database/Mysqli/Mysqli.php
@@ -5,19 +5,25 @@
  * @license    MIT
  */
 
-namespace Zoop\Juggernaut\Helper\Database;
+namespace Zoop\Juggernaut\Helper\Database\Mysqli;
 
+use \stdClass;
 use \Exception;
 use \mysqli as db;
 use Zoop\Juggernaut\Adapter\AdapterInterface;
+use Zoop\Juggernaut\Helper\Database\AbstractDatabase;
+use Zoop\Juggernaut\Helper\Database\DatabaseInterface;
 
-class Mysqli extends AbstractDatabase implements DatabaseInterface {
-
+class Mysqli extends AbstractDatabase implements DatabaseInterface
+{
     protected $cache = array();
     protected $connection;
+    protected $master;
+    protected $slaves = [];
     protected $transactionInProgress = false;
 
-    public function __construct(AdapterInterface $adapter = null, $logQueries = false) {
+    public function __construct(AdapterInterface $adapter = null, $logQueries = false)
+    {
         if (!is_null($adapter)) {
             $this->setAdapter($adapter);
         }
@@ -25,7 +31,8 @@ class Mysqli extends AbstractDatabase implements DatabaseInterface {
         $this->setLogQueries($logQueries);
     }
 
-    public function __destruct() {
+    public function __destruct()
+    {
         if ($this->displayErrors === true) {
             $errors = $this->getErrors();
             if (!empty($errors)) {
@@ -34,21 +41,173 @@ class Mysqli extends AbstractDatabase implements DatabaseInterface {
         }
     }
 
-    public function connect($host, $user, $password, $database, $port = 3306, $persistency = false) {
-        try {
-            $port = !empty($port) ? intval($port) : 3306;
-            $this->connection = new db($host, $user, $password, $database, $port);
-            if ($this->connection->connect_errno) {
-                die("Failed to connect to MySQL: " . $this->connection->connect_error);
-            } else {
-                $this->connection->set_charset("utf8");
-            }
-        } catch (Exception $e) {
-            die('Cannot connect to the database');
+    /**
+     * 
+     * @param string $host
+     * @param string $user
+     * @param string $password
+     * @param string $database
+     * @param string $port
+     * @param boolean $addMasterAsSlave
+     */
+    public function setMaster($host, $database, $user = null, $password = null, $port = 3306, $addMasterAsSlave = true)
+    {
+        $this->master = $this->connect($host, $database, $user, $password, $port);
+
+        //add the master as a slave as well
+        if ($addMasterAsSlave === true) {
+            $this->addSlave($host, $database, $user, $password, $port);
         }
     }
 
-    public function query($query, $ttl = 0) {
+    /**
+     * 
+     * @param type $host
+     * @param type $user
+     * @param type $password
+     * @param type $database
+     * @param type $port
+     */
+    public function addSlave($host, $database, $user, $password, $port = 3306)
+    {
+        $connection = new stdClass;
+
+        $connection->host = $host;
+        $connection->database = $database;
+        $connection->user = $user;
+        $connection->password = $password;
+        $connection->port = $port;
+
+        $this->slave[] = $connection;
+    }
+
+    /**
+     * 
+     * @param boolean $forceMaster
+     * @param boolean $slaveKey
+     * @return \mysqli
+     * @throws Exception Cannot conect to any slave
+     */
+    public function getConnection($forceMaster = false, $slaveKey = false)
+    {
+        if ($forceMaster === true) {
+            return $this->getMaster();
+        }
+
+        if (!isset($this->connection)) {
+            $slaves = $this->getSlaves();
+
+            if ($slaveKey === false) {
+                $slaveKey = array_rand($slaves);
+            }
+
+            $slave = $slaves[$slaveKey];
+            try {
+                $this->connection = $this->connect($slave->host, $slave->database, $slave->user, $slave->password, $slave->post);
+            } catch (Exception $e) {
+                //remove failed connection
+                unset($this->slaves[$slaveKey]);
+                $this->slaves = array_values($this->slaves);
+
+                if (!empty($this->slaves)) {
+                    return $this->getConnection($forceMaster, $slaveKey);
+                } else {
+                    throw new Exception('Cannot conect to any slave');
+                }
+            }
+        }
+
+        return $this->connection;
+    }
+
+    /**
+     * 
+     * @return \mysqli
+     */
+    public function getMaster()
+    {
+        return $this->master;
+    }
+
+    /**
+     * 
+     * @return array
+     */
+    public function getSlaves()
+    {
+        return $this->slaves;
+    }
+
+    /**
+     * 
+     * @return boolean
+     */
+    public function getTransactionInProgress()
+    {
+        return $this->transactionInProgress;
+    }
+
+    /**
+     * 
+     * @param \mysqli $connection
+     */
+    public function setConnection(\mysqli $connection)
+    {
+        $this->connection = $connection;
+    }
+
+    /**
+     * 
+     * @param type $slaves
+     */
+    public function setSlaves($slaves)
+    {
+        $this->slaves = $slaves;
+    }
+
+    /**
+     * 
+     * @param boolean $transactionInProgress
+     */
+    public function setTransactionInProgress($transactionInProgress)
+    {
+        $this->transactionInProgress = (bool) $transactionInProgress;
+    }
+
+    /**
+     * 
+     * @param type $host
+     * @param type $user
+     * @param type $password
+     * @param type $database
+     * @param type $port
+     * @return \mysqli
+     */
+    private function connect($host, $database, $user = null, $password = null, $port = 3306)
+    {
+        try {
+            $port = !empty($port) ? intval($port) : 3306;
+            $connection = new db($host, $user, $password, $database, $port);
+            if ($connection->connect_errno) {
+                throw new Exception('Cannot connect to ' . $host . '. Error: ' . $connection->connect_errno);
+                return false;
+            } else {
+                $connection->set_charset("utf8");
+            }
+            return $connection;
+        } catch (Exception $e) {
+            throw new Exception('Cannot connect to ' . $host);
+        }
+    }
+
+    /**
+     * 
+     * @param string $query
+     * @param int $ttl
+     * @return \mysqli_result
+     */
+    public function query($query, $ttl = 0)
+    {
         $time = microtime(true);
         $cached = true;
         $ttl = intval($ttl);
@@ -57,28 +216,29 @@ class Mysqli extends AbstractDatabase implements DatabaseInterface {
                 strpos($query, 'INSERT') !== false ||
                 strpos($query, 'UPDATE') !== false ||
                 strpos($query, 'DELETE') !== false ||
-                $this->transactionInProgress === true ||
+                $this->getTransactionInProgress() === true ||
                 $ttl == 0
         ) {
-            $r = ($this->connection) ? $this->connection->query($query) : false;
+            //only use master for writes
+            $r = ($this->getMaster()) ? $this->getMaster()->query($query) : false;
             if ($r === false) {
                 $this->addError($query);
             }
         } else {
-            if (!is_null($this->adapter)) {
-                $this->adapter->setTtl($ttl);
+            if (!is_null($this->getAdapter())) {
+                $this->getAdapter()->setTtl($ttl);
 
-                $r = $this->adapter->getItem($query, $success);
+                $r = $this->getAdapter()->getItem($query, $success);
                 if ($success === true) {
                     $this->setCache($query, $r);
                 } else {
-                    $r = ($this->connection) ? $this->connection->query($query) : false;
+                    $r = ($this->getConnection()) ? $this->getConnection()->query($query) : false;
                     if ($r !== false) {
                         $this->setCache($query, $r);
                     } else {
                         $this->addError($query);
                         //clear the queue to ensure no lingering queues which will prevent the next load
-                        $this->adapter->clearQueue($query);
+                        $this->getAdapter()->clearQueue($query);
                     }
                     $cached = false;
                 }
@@ -86,9 +246,9 @@ class Mysqli extends AbstractDatabase implements DatabaseInterface {
                 //set the resource id to the query so we can pull it from the file cache
                 unset($r);
                 $r = $query;
-                $this->adapter->normalizeKey($r);
+                $this->getAdapter()->normalizeKey($r);
             } else {
-                $r = ($this->connection) ? $this->connection->query($query) : false;
+                $r = ($this->getConnection()) ? $this->getConnection()->query($query) : false;
                 if ($r === false) {
                     $this->addError($query);
                 }
@@ -96,7 +256,7 @@ class Mysqli extends AbstractDatabase implements DatabaseInterface {
         }
 
         $executionTime = (microtime(true) - $time);
-        if ($this->logQueries === true) {
+        if ($this->getLogQueries() === true) {
             $this->log[] = array(
                 'time' => $executionTime,
                 'query' => $query,
@@ -108,14 +268,20 @@ class Mysqli extends AbstractDatabase implements DatabaseInterface {
         return $r;
     }
 
-    private function setCache($key, $result) {
+    /**
+     * 
+     * @param string $key
+     * @param object|\mysqli_result $result
+     */
+    private function setCache($key, $result)
+    {
         $rawKey = $key;
-        $this->adapter->normalizeKey($key);
+        $this->getAdapter()->normalizeKey($key);
         if (!isset($this->cache[$key])) {
             if (gettype($result) == 'object' && is_a($result, 'mysqli_result')) {
                 $data = $this->getAllRows($result);
 
-                $this->adapter->setItem($rawKey, $data);
+                $this->getAdapter()->setItem($rawKey, $data);
                 $this->cache[$key] = $data;
             } else {
                 $this->cache[$key] = $result;
@@ -127,7 +293,13 @@ class Mysqli extends AbstractDatabase implements DatabaseInterface {
         }
     }
 
-    private function getAllRows($result) {
+    /**
+     * 
+     * @param \mysqli_result $result
+     * @return array
+     */
+    private function getAllRows($result)
+    {
         $data = array();
         while ($row = $this->fetchRow($result)) {
             $data[] = $row;
@@ -135,11 +307,23 @@ class Mysqli extends AbstractDatabase implements DatabaseInterface {
         return $data;
     }
 
-    public function numberOfRows($result) {
+    /**
+     * 
+     * @param \mysqli_result $result
+     * @return int
+     */
+    public function numberOfRows($result)
+    {
         return ($result) ? $result->num_rows : 0;
     }
 
-    public function fetchRow($result) {
+    /**
+     * 
+     * @param object|string $result
+     * @return string|boolean
+     */
+    public function fetchRow($result)
+    {
         if (gettype($result) == 'object') {
             return ($result) ? $result->fetch_array(MYSQLI_ASSOC) : false;
         } else if (is_string($result)) {
@@ -154,19 +338,23 @@ class Mysqli extends AbstractDatabase implements DatabaseInterface {
         }
     }
 
-    public function getInsertedId() {
-        return ($this->connection) ? $this->connection->insert_id : false;
+    public function getInsertedId()
+    {
+        return ($this->getConnection(true)) ? $this->getConnection(true)->insert_id : false;
     }
 
-    public function escape($string) {
-        return ($this->connection) ? $this->connection->real_escape_string($string) : $string;
+    public function escape($string)
+    {
+        return ($this->getConnection(true)) ? $this->getConnection(true)->real_escape_string($string) : $string;
     }
 
-    public function affectedRows() {
-        return ($this->connection) ? $this->connection->affected_rows : false;
+    public function affectedRows()
+    {
+        return ($this->getConnection(true)) ? $this->getConnection(true)->affected_rows : false;
     }
 
-    public function getFields($result) {
+    public function getFields($result)
+    {
         if (gettype($result) == 'object') {
             return $result->fetch_fields();
         } else if (is_array($result) && !empty($result)) {
@@ -176,44 +364,57 @@ class Mysqli extends AbstractDatabase implements DatabaseInterface {
         return false;
     }
 
-    public function transaction($status = 'begin') {
+    public function transaction($status = 'begin')
+    {
         switch ($status) {
             case 'begin':
-                $this->transactionInProgress = true;
-                return $this->connection->autocommit(false);
+                $this->setTransactionInProgress(true);
+                return $this->getConnection(true)->autocommit(false);
                 break;
-
             case 'commit':
-                $this->transactionInProgress = false;
-                return $this->connection->commit();
+                $this->setTransactionInProgress(false);
+                return $this->getConnection(true)->commit();
                 break;
-
             case 'rollback':
-                $this->transactionInProgress = false;
-                return $this->connection->rollback();
+                $this->setTransactionInProgress(false);
+                return $this->getConnection(true)->rollback();
                 break;
         }
 
         return true;
     }
 
-    public function beginTransaction() {
-        $this->transactionInProgress = true;
-        return $this->connection->autocommit(false);
+    public function beginTransaction()
+    {
+        $this->setTransactionInProgress(true);
+        return $this->getConnection(true)->autocommit(false);
     }
 
-    public function commitTransaction() {
-        $this->transactionInProgress = false;
-        return $this->connection->commit();
+    public function commitTransaction()
+    {
+        $this->setTransactionInProgress(false);
+        return $this->getConnection(true)->commit();
     }
 
-    public function rollbackTransaction() {
-        $this->transactionInProgress = false;
-        return $this->connection->rollback();
+    public function rollbackTransaction()
+    {
+        $this->setTransactionInProgress(false);
+        return $this->getConnection(true)->rollback();
     }
 
-    public function close() {
-        return (get_class($this->connection) == 'mysqli') ? @$this->connection->close() : false;
+    public function close()
+    {
+        $slave = $this->getSlaves();
+        $master = $this->getMaster();
+
+        if (get_class($slave) == 'mysqli') {
+            @$slave->close();
+        }
+
+        if (get_class($master) == 'mysqli') {
+            @$master->close();
+        }
+        return true;
     }
 
 }
